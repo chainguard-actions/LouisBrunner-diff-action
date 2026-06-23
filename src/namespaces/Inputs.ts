7339fdbcaf0e202e160426c34677e9650c418649
actionchain <actionchain@chainguard.dev>
2026-06-23 10:25:24 +0000

    Update LouisBrunner/diff-action@v2.1.0

---
+++ src/namespaces/Inputs.ts
@@ -0,0 +1,31 @@
+export type Args = {
+	old: string;
+	new: string;
+
+	tolerance: Tolerance;
+	mode: Mode;
+	output?: string;
+
+	notifications?: {
+		token: string;
+		label?: string;
+		issue: boolean;
+		check: boolean;
+		sticky: boolean;
+	};
+};
+
+export enum Tolerance {
+	Better = "better",
+	MixedBetter = "mixed-better",
+	Same = "same",
+	Mixed = "mixed",
+	MixedWorse = "mixed-worse",
+	Worse = "worse",
+}
+
+export enum Mode {
+	Strict = "strict",
+	Addition = "addition",
+	Deletion = "deletion",
+}
